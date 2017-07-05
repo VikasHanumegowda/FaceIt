6fc78e2ab4c8376ffc1e3c71e70a2bdd5b27d115
vikashanumegowda <hanmego@usc.edu>
2017-07-04 18:37:09 -0700

    Mouth rect-area done

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class FaceView: UIView {
 
     @IBInspectable
@@ -54,6 +55,21 @@ class FaceView: UIView {
         return path
     }
     
+    private func pathForMouth() -> UIBezierPath {
+        let mouthWidth = skullRadius/Ratios.skullRadiusToMouthWidth
+        let mouthHeight = skullRadius/Ratios.skullRadiusToMouthHeight
+        let mouthOffset = skullRadius/Ratios.skullRadiusToMouthOffset
+        
+        let mouthRect = CGRect(
+            x: skullCenter.x-mouthWidth/2,
+            y: skullCenter.y+mouthOffset,
+            width: mouthWidth,
+            height: mouthHeight)
+        
+        let path = UIBezierPath(rect: mouthRect)
+        return path
+    }
+    
     private func pathForSkull() -> UIBezierPath {
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
         path.lineWidth = 5.0
@@ -66,6 +82,7 @@ class FaceView: UIView {
         UIColor.black.set()
         pathForEye(.left).stroke()
         pathForEye(.right).stroke()
+        pathForMouth().stroke()
         
     }
     
