c2d3d27c48b2a209c76dcda15daeb4f6747db7b0
vikashanumegowda <hanmego@usc.edu>
2017-07-12 21:40:11 -0700

    Added a model to control the view - FaceView

--- FaceIt/ViewController.swift
+++ FaceIt/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var faceView: FaceView!{
+        didSet{
+            updateUI()
+        }
+    }
     
+    var expression = FacialExpression(eyes: .Closed, mouth: .Frown){
+        didSet{
+            updateUI()
+        }
+    }
+    
+    private func updateUI(){
+        switch expression.eyes {
+        case .Open:
+            faceView?.eyesOpen = true
+        case .Closed:
+            faceView?.eyesOpen = false
+        case .Squinting:
+            faceView?.eyesOpen = false
+        }
+        faceView?.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+    }
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.Grin:0.5,.Frown:-1.0,.Smirk:-0.5,.Neutral:0.0,.Smile:1.0]
 }
 
